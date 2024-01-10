8a3468b8b344e6fd9b4637242b2662aca627bd02
carnol16 <74794392+carnol16@users.noreply.github.com>
2024-01-10 16:08:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ $('#chooseFile').bind('change', function () {
   var filename = $("#chooseFile").val();
   if (/^\s*$/.test(filename)) {
     $(".file-upload").removeClass('active');
-    $("#noFile").text("No file chosen..."); 
   }
   else {
     $(".file-upload").addClass('active');
